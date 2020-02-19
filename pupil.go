4f56d9baf8b018040a365c32577bc9f4e8316335
Andrey Shcherbinin <jynweythek223@gmail.com>
2020-02-19 09:46:06 +0300

    add comments to pupil

--- pupil.go
+++ pupil.go
@@ -1,9 +1,12 @@
 package garbage
 
+// PupilID uniquely identifies a pupil
 type PupilID string
 
+// Pupil brings recyclables to events
 type Pupil struct {
-	ID        PupilID
+	ID PupilID
+	// A pupil can be moved between classes at any given moment
 	Class     *Class
 	FirstName string
 	LastName  string
