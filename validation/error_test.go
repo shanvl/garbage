5a32ac2bbd504bbea5612a163d3b06b275bfa0a6
Andrey Shcherbinin <jynweythek223@gmail.com>
2020-02-20 10:26:08 +0300

    add validation errors test

--- validation/error_test.go
+++ validation/error_test.go
@@ -6,25 +6,33 @@ import (
 )
 
 func TestErrValidation_Error(t *testing.T) {
+	// We should consider in the tests that a map's values order is always random,
+	// that's why there are 2 cases of "want" — they cover every possible combination of map's values.
+	// Another way to solve this would be to sort map's keys in the Error function itself,
+	// before returning them as a string, but I don't see any practical value in it for a client
 	tests := []struct {
 		name   string
 		fields map[string]string
 		want   string
+		want2  string
 	}{
 		{
 			name:   "no fields",
 			fields: nil,
 			want:   "",
+			want2:  "",
 		},
 		{
 			name:   "1 field",
 			fields: map[string]string{"name": "wrong name"},
 			want:   "name: wrong name\n",
+			want2:  "name: wrong name\n",
 		},
 		{
 			name:   "2 fields",
 			fields: map[string]string{"name": "wrong name", "age": "wrong age"},
 			want:   "name: wrong name\nage: wrong age\n",
+			want2:  "age: wrong age\nname: wrong name\n",
 		},
 	}
 	for _, tt := range tests {
@@ -32,13 +40,14 @@ func TestErrValidation_Error(t *testing.T) {
 			e := &ErrValidation{
 				fields: tt.fields,
 			}
-			if got := e.Error(); got != tt.want {
-				t.Errorf("Error() = %v, want %v", got, tt.want)
+			if got := e.Error(); got != tt.want && got != tt.want2 {
+				t.Errorf("Error() = %v, want %v, want2 %v", got, tt.want, tt.want2)
 			}
 		})
 	}
 }
 
+// genius
 func TestErrValidation_Fields(t *testing.T) {
 	tests := []struct {
 		name   string
@@ -68,26 +77,35 @@ func TestErrValidation_Fields(t *testing.T) {
 	}
 }
 
-// func TestErrValidation_add(t *testing.T) {
-// 	type fields struct {
-// 		fields map[string]string
-// 	}
-// 	type args struct {
-// 		field string
-// 		err   string
-// 	}
-// 	tests := []struct {
-// 		name   string
-// 		fields fields
-// 		args   args
-// 	}{
-// 		// TODO: Add test cases.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			e := &ErrValidation{
-// 				fields: tt.fields.fields,
-// 			}
-// 		})
-// 	}
-// }
+func TestErrValidation_add(t *testing.T) {
+	tests := []struct {
+		name string
+		args [][]string
+		want map[string]string
+	}{
+		{
+			name: "no args",
+			args: [][]string{},
+			want: nil,
+		},
+		{
+			name: "2 args",
+			args: [][]string{
+				{"name", "wrong name"},
+				{"age", "wrong age"},
+			},
+			want: map[string]string{"name": "wrong name", "age": "wrong age"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &ErrValidation{}
+			for _, arg := range tt.args {
+				e.add(arg[0], arg[1])
+			}
+			if got := e.fields; !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("e.fields = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
