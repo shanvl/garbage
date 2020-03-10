62090963c95f3d952867cf526550ad1d22865df2
Andrey Shcherbinin <jynweythek223@gmail.com>
2020-03-10 19:37:55 +0300

    move sorting to the separate package

---
+++ sort/by.go
@@ -0,0 +1,42 @@
+// Package sort specifies how various entities can be sorted
+package sort
+
+// By is a type indicating how things can be sorted
+type By string
+
+const (
+	DateAsc By = "dateAsc"
+	DateDes By = "dateDes"
+	Gadgets By = "gadgets"
+	NameAsc By = "nameAsc"
+	NameDes By = "nameDes"
+	Paper   By = "paper"
+	Plastic By = "plastic"
+)
+
+// IsValid checks if a provided string can be used for sorting
+func (s By) IsValid() bool {
+	switch s {
+	case DateAsc, DateDes, Gadgets, Paper, Plastic, NameAsc, NameDes:
+		return true
+	}
+	return false
+}
+
+// IsForEventPupils checks if a provided string can be used to sort an event's pupils
+func (s By) IsForEventPupils() bool {
+	switch s {
+	case NameAsc, NameDes, Gadgets, Paper, Plastic:
+		return true
+	}
+	return false
+}
+
+// IsForEventPupils checks if a provided string can be used to sort an event's classes
+func (s By) IsForEventClasses() bool {
+	switch s {
+	case NameAsc, NameDes, Gadgets, Paper, Plastic:
+		return true
+	}
+	return false
+}
---
+++ sort/by_test.go
@@ -0,0 +1,108 @@
+package sort
+
+import (
+	"testing"
+)
+
+func TestSort_IsValid(t *testing.T) {
+	tests := []struct {
+		name string
+		s    By
+		want bool
+	}{
+		{
+			name: "valid input",
+			s:    Plastic,
+			want: true,
+		},
+		{
+			name: "empty string",
+			s:    "",
+			want: false,
+		},
+		{
+			name: "invalid input",
+			s:    "invalid",
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.IsValid(); got != tt.want {
+				t.Errorf("IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSort_IsForEventPupils(t *testing.T) {
+	tests := []struct {
+		name string
+		s    By
+		want bool
+	}{
+		{
+			name: "valid input",
+			s:    Plastic,
+			want: true,
+		},
+		{
+			name: "empty string",
+			s:    "",
+			want: false,
+		},
+		{
+			name: "invalid sorting",
+			s:    DateDes,
+			want: false,
+		},
+		{
+			name: "invalid input",
+			s:    "invalid",
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.IsForEventPupils(); got != tt.want {
+				t.Errorf("IsForEventPupils() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSort_IsForEventClasses(t *testing.T) {
+	tests := []struct {
+		name string
+		s    By
+		want bool
+	}{
+		{
+			name: "valid input",
+			s:    Plastic,
+			want: true,
+		},
+		{
+			name: "empty string",
+			s:    "",
+			want: false,
+		},
+		{
+			name: "invalid sorting",
+			s:    DateDes,
+			want: false,
+		},
+		{
+			name: "invalid input",
+			s:    "invalid",
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.IsForEventClasses(); got != tt.want {
+				t.Errorf("IsForEventClasses() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
