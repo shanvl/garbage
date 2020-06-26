65bb2410fae7aecde616cdc7bbadb3fd9b0f1c3c
Andrey Shcherbinin <jynweythek223@gmail.com>
2020-06-27 00:11:30 +0300

    move proto maps to their own files

--- internal/eventssvc/grpc/eventing.go
+++ internal/eventssvc/grpc/eventing.go
@@ -7,7 +7,6 @@ import (
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/empty"
 	eventsv1pb "github.com/shanvl/garbage/api/events/v1/pb"
-	"github.com/shanvl/garbage/internal/eventssvc"
 	"github.com/shanvl/garbage/internal/eventssvc/eventing"
 )
 
@@ -180,59 +179,5 @@ func pupilToProto(pupil *eventing.Pupil) *eventsv1pb.Pupil {
 		LastName:         pupil.LastName,
 		Class:            pupil.Class,
 		ResourcesBrought: resourceMapToProto(pupil.ResourcesBrought),
-	}
-}
-
-var resourceProtoMap = map[eventssvc.Resource]eventsv1pb.Resource{
-	eventssvc.Gadgets: eventsv1pb.Resource_RESOURCE_GADGETS,
-	eventssvc.Paper:   eventsv1pb.Resource_RESOURCE_PAPER,
-	eventssvc.Plastic: eventsv1pb.Resource_RESOURCE_PLASTIC,
-}
-
-var protoResourceMap = map[eventsv1pb.Resource]eventssvc.Resource{
-	eventsv1pb.Resource_RESOURCE_GADGETS: eventssvc.Gadgets,
-	eventsv1pb.Resource_RESOURCE_PAPER:   eventssvc.Paper,
-	eventsv1pb.Resource_RESOURCE_PLASTIC: eventssvc.Plastic,
-}
-
-// converts []eventssvc.Resource to []eventsv1pb.Resource
-func resourcesToProto(resources []eventssvc.Resource) []eventsv1pb.Resource {
-	proto := make([]eventsv1pb.Resource, len(resources))
-	for i, res := range resources {
-		proto[i] = resourceProtoMap[res]
-	}
-	return proto
-}
-
-// converts []eventsv1pb.Resource to []eventssvc.Resource
-func protoToResources(proto []eventsv1pb.Resource) ([]eventssvc.Resource, error) {
-	resources := make([]eventssvc.Resource, len(proto))
-	for i, res := range proto {
-		if res == eventsv1pb.Resource_RESOURCE_UNKNOWN {
-			return nil, eventssvc.ErrUnknownResource
-		}
-		resources[i] = protoResourceMap[res]
-	}
-	return resources, nil
-}
-
-// converts *eventsv1pb.ResourcesBrought to eventssvc.ResourceMap
-func protoToResourcesMap(proto *eventsv1pb.ResourcesBrought) eventssvc.ResourceMap {
-	if proto == nil {
-		return eventssvc.ResourceMap{}
-	}
-	return eventssvc.ResourceMap{
-		eventssvc.Gadgets: proto.Gadgets,
-		eventssvc.Paper:   proto.Paper,
-		eventssvc.Plastic: proto.Plastic,
-	}
-}
-
-// converts eventssvc.ResourceMap to *eventsv1pb.ResourcesBrought
-func resourceMapToProto(m eventssvc.ResourceMap) *eventsv1pb.ResourcesBrought {
-	return &eventsv1pb.ResourcesBrought{
-		Gadgets: m[eventssvc.Gadgets],
-		Paper:   m[eventssvc.Paper],
-		Plastic: m[eventssvc.Plastic],
 	}
 }
