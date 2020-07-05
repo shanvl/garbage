79eb8e71ea8953f93569fd0474c94e65952ac769
Andrey Shcherbinin <jynweythek223@gmail.com>
2020-07-05 16:08:34 +0300

    add authsvc ChangeUserRole tests

--- internal/authsvc/users/service_test.go
+++ internal/authsvc/users/service_test.go
@@ -36,6 +36,13 @@ func Test_service_CreateUser(t *testing.T) {
 			wantErr: true,
 		},
 		{
+			name: "long email",
+			args: args{
+				email: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@aaa.com",
+			},
+			wantErr: true,
+		},
+		{
 			name: "repo's error",
 			args: args{
 				email: repoError,
@@ -121,11 +128,31 @@ func Test_service_ActivateUser(t *testing.T) {
 			wantErr: true,
 		},
 		{
+			name: "too long first name",
+			args: args{
+				activationToken: activationToken,
+				firstName:       "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+				lastName:        "ln",
+				password:        "psw",
+			},
+			wantErr: true,
+		},
+		{
 			name: "no last name",
 			args: args{
 				activationToken: activationToken,
 				firstName:       "fn",
 				lastName:        "",
+				password:        "psw",
+			},
+			wantErr: true,
+		},
+		{
+			name: "too long last name",
+			args: args{
+				activationToken: activationToken,
+				firstName:       "fn",
+				lastName:        "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
 				password:        "psw",
 			},
 			wantErr: true,
@@ -137,6 +164,16 @@ func Test_service_ActivateUser(t *testing.T) {
 				firstName:       "fn",
 				lastName:        "ln",
 				password:        "",
+			},
+			wantErr: true,
+		},
+		{
+			name: "no password",
+			args: args{
+				activationToken: activationToken,
+				firstName:       "fn",
+				lastName:        "ln",
+				password:        "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
 			},
 			wantErr: true,
 		},
@@ -170,6 +207,72 @@ func Test_service_ActivateUser(t *testing.T) {
 			}
 			if err == nil && id == "" {
 				t.Errorf("ActivateUser() error == nil, len(userID) == 0")
+			}
+		})
+	}
+}
+
+func Test_service_ChangeUserRole(t *testing.T) {
+	ctx := context.Background()
+	const repoError = "repo error"
+	repo := &mock.UsersRepo{}
+	repo.UserByIDFn = func(ctx context.Context, id string) (*authsvc.User, error) {
+		if id == repoError {
+			return nil, errors.New("error")
+		}
+		return &authsvc.User{
+			ID:     "id",
+			Active: false,
+			Email:  "email",
+			Role:   authsvc.Member,
+		}, nil
+	}
+	repo.StoreUserFn = func(ctx context.Context, user *authsvc.User) error {
+		if user.Email == repoError {
+			return errors.New("error")
+		}
+		return nil
+	}
+	s := NewService(repo)
+	type args struct {
+		id   string
+		role authsvc.Role
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "no id",
+			args: args{
+				id:   "",
+				role: authsvc.Member,
+			},
+			wantErr: true,
+		},
+		{
+			name: "repo error",
+			args: args{
+				id:   repoError,
+				role: authsvc.Member,
+			},
+			wantErr: true,
+		},
+		{
+			name: "no id",
+			args: args{
+				id:   "id",
+				role: authsvc.Member,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := s.ChangeUserRole(ctx, tt.args.id, tt.args.role)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ChangeUserRole() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
